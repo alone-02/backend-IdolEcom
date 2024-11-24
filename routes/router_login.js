84295c7b20e1525c68f99043b282510e29bacf69
Alone <santoshbhaskal2127@gmail.com>
2024-11-24 14:56:42 +0530

    some modify

--- routes/router_login.js
+++ routes/router_login.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const signupUser = require("../models/model_user");
 const jwt = require("jsonwebtoken");
 const env = require("dotenv");
--- routes/router_signUp.js
+++ routes/router_signUp.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require("../models/model_user");
 
 const router = express.Router();
